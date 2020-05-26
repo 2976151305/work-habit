c698aca2e278da3d81d1671e16b25f56c92bb46f
unknown <2976151305@qq.com>
2020-05-26 17:02:35 +0800

    update

---
+++ 自定义utils/tt.utils.js
@@ -0,0 +1,119 @@
+const formatTime = date => {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+  
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  }
+  
+  const formatNumber = n => {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+  }
+  
+  /**
+   * toast提示
+   * @param {String} title 提示内容
+   * @param {Number}} duration 持续时间
+   * @param {String} icon 显示图标
+   */
+  const toast = (title, duration = 2000, icon = 'none') => {
+    tt.showToast({
+      title,
+      duration,
+      icon
+    })
+  }
+  
+  /**
+   * 模态框
+   * @param {String} title 标题
+   * @param {String} content 内容
+   * @param {Object} confirmConfig 确认按钮设置
+   * @param {Object} cancelConfig 取消按钮设置
+   * @param {Function} successCallBack 成功回调
+   * @param {Function} failCallBack 失败回调
+   */
+  const showModal = (title, content, confirmConfig, cancelConfig, successCallBack = () => {}, failCallBack = () => {}) => {
+    tt.showModal({
+      title,
+      content,
+      ...confirmConfig,
+      ...cancelConfig,
+      success: res => {
+        if (res.confirm) successCallBack()
+        else failCallBack()
+      },
+      fail: err => {
+        throw new Error(err)
+      }
+    })
+  }
+  
+  /**
+   * 创建动画
+   * @param {Number} duration 持续时间
+   * @param {Number} delay 持续时间
+   * @param {String} timingFunction 动画效果
+   * @param {String} transformOrigin 重心位置
+   * @returns
+   */
+  const createAnimation = (duration = 300, delay = 0, timingFunction = 'linear', transformOrigin = '50% 50% 0') => {
+    const animation = tt.createAnimation({
+      duration,
+      delay,
+      timingFunction,
+      transformOrigin
+    })
+    return animation
+  }
+  
+  /**
+   * 设置缓存
+   * @param {String} name 缓存key
+   * @param {} data 缓存数据
+   */
+  const setStorage = (name, data) => {
+    if (data instanceof Object) data = JSON.stringify(data)
+    tt.setStorageSync(name, data)
+  }
+  
+  /**
+   * 读取缓存
+   * @param {String}} name 缓存key
+   * @returns
+   */
+  const getStorage = name => {
+    let res = tt.getStorageSync(name)
+    if (res.indexOf('{') >= 0) res = JSON.parse(res)
+    return res
+  }
+  
+  /**
+   * 创建对象实例
+   * @param {String} el 对象选择器
+   * @returns
+   */
+  const createSelectorQuery = (el) => {
+    return new Promise((resolve, reject) => {
+      const query = tt.createSelectorQuery()
+      query.select(el).boundingClientRect()
+      query.exec(res => {
+        resolve(res[0])
+      })
+    })
+  }
+  
+  module.exports = {
+    formatTime,
+    toast,
+    showModal,
+    createAnimation,
+    getStorage,
+    setStorage,
+    createSelectorQuery
+  }
+  
---
+++ 自定义utils/wx.request.js
@@ -0,0 +1,36 @@
+import { toast } from './util'
+const { BASEURL } = getApp().globalData
+
+/**
+ * 发起请求
+ * @param {String} url 请求URL
+ * @param {String} method 请求方法
+ * @param {Object} data 请求参数
+ * @param {Object} header 请求头
+ * @returns
+ */
+const request = (url, method, data, header) => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: BASEURL + url,
+      method,
+      data,
+      header,
+      success: res => {
+        if (res.statusCode === 200) resolve(res)
+        else {
+          toast('出错了')
+          reject(err)
+        }
+      },
+      fail: err => {
+        toast('出错了')
+        reject(err)
+      }
+    })
+  })
+}
+
+module.exports = {
+  request
+}
